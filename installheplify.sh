e639f72300c3f0b6fadeb63b4a628ea8ab870f25
Andrei Ishbuldin <43541101+ishbuldin@users.noreply.github.com>
2019-12-10 12:29:44 +0500

    Update installheplify.sh

--- installheplify.sh
+++ installheplify.sh
@@ -5,6 +5,7 @@ wget https://github.com/sipcapture/heplify/releases/download/1.53/heplify
 chmod +x heplify
 cd /etc/systemd/system/
 wget https://raw.githubusercontent.com/ishbuldin/hello-world/master/heplify.service
+sed -i "s/{HOSTNAME}/${HOSTNAME}/g" heplify.service
 systemctl daemon-reload
 systemctl start heplify
 systemctl enable heplify
